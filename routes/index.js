42a72033c87723ba5cfe0b5213123c0fca837bfb
Shashi Shekhar <shashi.sh@media.net>
2016-10-16 01:20:43 +0530

    getYearWiseData api exposed

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var getYearWiseData = require('../utilities/apiHelpers').getYearWiseData;
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getYearWiseData', function(req, res, next) {
+  getYearWiseData().then(function (data) {
+    res.send(data);
+  })
+});
 
 
 module.exports = router;
